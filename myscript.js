379ce5a15b3814ab3bc31e3f3ac2bbe8579419b1
LadoGelava <56440841+LadoGelava@users.noreply.github.com>
2019-11-01 17:13:45 +0400

    dsf

---
+++ myscript.js
@@ -0,0 +1,12 @@
+﻿
+	function scrollWin() {
+  	window.scrollBy(0, 2); // horizontal and vertical scroll 					increments
+  	scrolldelay = setTimeout('scrollWin()', 1); // scrolls 						every 100 milliseconds
+
+	 if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+	
+	clearTimeout(scrolldelay);
+    scrolldelay = setTimeout('PageUp()', 2000);
+	}
+	}
+ 
