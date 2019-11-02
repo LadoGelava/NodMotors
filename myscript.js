2826eace4820c8ad2e36473d7181e822e5aa9e81
LadoGelava <56440841+LadoGelava@users.noreply.github.com>
2019-11-02 22:06:54 +0400

    adsa

--- myscript.js
+++ myscript.js
@@ -1,12 +1,29 @@
 ﻿
 	function scrollWin() {
-  	window.scrollBy(0, 2); // horizontal and vertical scroll 					increments
-  	scrolldelay = setTimeout('scrollWin()', 1); // scrolls 						every 100 milliseconds
-
-	 if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
-	
-	clearTimeout(scrolldelay);
-    scrolldelay = setTimeout('PageUp()', 2000);
-	}
+  	window.scrollBy(0, 3000); //vertical scroll
+  	
 	}
  
+
+	$(document).ready(function(){
+		$('#rotate').click(function(){
+			$(this).toggleClass('open');
+		});
+	});
+
+
+	
+
+	function burgerClick(){
+		var x = document.getElementById("hero");
+  if (x.style.opacity === "0.03") {
+    x.style.opacity = "1";
+  } else {
+    x.style.opacity = "0.03";
+  }
+}
+		
+
+	
+
+	
