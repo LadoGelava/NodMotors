81821c5175f2d8da48076a23dec1aae9f516a20d
LadoGelava <56440841+LadoGelava@users.noreply.github.com>
2019-11-09 19:03:07 +0400

    scroll down fixed

--- myscript.js
+++ myscript.js
@@ -1,9 +1,15 @@
 ﻿
-	function scrollWin() {
-  	window.scrollBy(0, 3000); //vertical scroll
-  	
-	}
- 
+function scrollWin() {
+	window.scrollBy(0, 2); // horizontal and vertical scroll 					increments
+	scrolldelay = setTimeout('scrollWin()', 1); // scrolls 						every 100 milliseconds
+
+ if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+
+clearTimeout(scrolldelay);
+	scrolldelay = setTimeout('PageUp()', 2000);
+}
+}
+
 
 	$(document).ready(function(){
 		$('#rotate').click(function(){
